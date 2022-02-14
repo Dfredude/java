ad63b9ba8d3db48c68b035ce14898f1b4ef2383c
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-02-13 22:15:41 -0800

    Read Input excercise

---
+++ missions/src/Chapter06/dReadInput.java
@@ -0,0 +1,17 @@
+package Chapter06;
+
+public class dReadInput {
+	public static void main(String numbers[]) {
+		//4 strings are taken as "numbers" arguments
+		float num_1 = Float.parseFloat(numbers[0]);
+		float num_2 = Float.parseFloat(numbers[1]);
+		float num_3 = Float.parseFloat(numbers[2]);
+		float num_4 = Float.parseFloat(numbers[3]);
+		System.out.println("I like these numbers: ");
+		System.out.println(num_1);
+		System.out.println(num_2);
+		System.out.println(num_3);
+		System.out.println(num_4);
+	}
+
+}
