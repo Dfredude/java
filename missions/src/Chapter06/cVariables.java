83a14aab1c48b54d6e711fa1a1a5a38e46b98c64
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-02-13 21:23:21 -0800

    3rd excercise

---
+++ missions/src/Chapter06/cVariables.java
@@ -0,0 +1,15 @@
+package Chapter06;
+
+public class cVariables {
+	public static void main(String args[]) {
+		int my_num = 5;
+		String my_string = "I got ";
+		char my_char = 'A';
+		boolean my_bool = true;
+		byte my_byte = 127;
+		float my_float = 84.231f;
+		System.out.println(my_string+my_num+" "+my_char+"'s"+" in school");
+		System.out.println("It's "+my_bool+" that a byte's max value is: "+my_byte+"\nWhereas a float's got decimals such as in: "+my_float);
+	}
+
+}
