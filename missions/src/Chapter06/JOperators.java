f6827003eec0bb4b92d84cf1e4594e64aed3ff81
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-02-14 15:21:46 -0800

    Add: 06-j

---
+++ missions/src/Chapter06/JOperators.java
@@ -0,0 +1,17 @@
+package Chapter06;
+
+public class JOperators {
+
+	public static void main(String[] args) {
+		System.out.println("• Using ~ with 5, 88, -456: "+ ~5 +", "+~88+", "+ ~-456);
+		System.out.println("It turns 0 bits to 1 and viceversa.");
+		System.out.println("• << (Shifting bits left by one) with 5: ");
+		System.out.println(5<<1);
+		System.out.println("• >> (Shifting signed bits right by two) with 5: ");
+		System.out.println(6>>2);
+		System.out.println("• >>> (Shifting extension bits right by two) with 5: ");
+		System.out.println(6>>2);
+
+	}
+
+}
